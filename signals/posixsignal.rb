abcd0509ba5f0ed9b2d487183746abf9c63bf5d3
kubo39 <kubo39@gmail.com>
2015-01-12 08:53:43 +0900

    Update

--- signals/posixsignal.rb
+++ signals/posixsignal.rb
@@ -1,5 +1,6 @@
 require 'ffi'
 
+
 module Unistd
   extend FFI::Library
   ffi_lib 'libc.so.6'
@@ -25,14 +26,51 @@ module SignalExt
 
   # int sigismember(const sigset_t*, int);
   attach_function :sigismember, [:pointer, :int], :int
+
+  # int sigemptyset(sigset_t*);
+  attach_function :sigemptyset, [:pointer], :int
+
+  # int sigaddset(sigset_t*, int);
+  attach_function :sigaddset, [:pointer, :int], :int
+
+  # int sigpending(sigset_t*);
+  attach_function :sigpending, [:pointer], :int
 end
 
+
 module Signal
+  SIG_BLOCK = 0
+  SIG_UNBLOCK = 1
+  SIG_SETMASK = 2
+
   class SigprocmaskError < Exception; end
+  class SigpendingError < Exception; end
+
+  Sigset = SignalExt::Sigset
 
   def self.sigprocmask(how, set, oldset)
-    if SignalExt.sigprocmask(how, set, oldset) < 0
+    if SignalExt.sigprocmask(how, set, oldset) != 0
       raise SigprocmaskError
     end
+    return 0
+  end
+
+  def self.sigismember(set, signo)
+    SignalExt.sigismember(set, signo)
+  end
+
+  def self.sigemptyset(set)
+    SignalExt.sigemptyset(set)
+  end
+
+  def self.sigaddset(set, signo)
+    SignalExt.sigaddset(set, signo)
+  end
+
+  def self.sigpending(set)
+    if SignalExt.sigpending(set) != 0
+      raise SigpendingError
+    end
+    return 0
   end
 end
