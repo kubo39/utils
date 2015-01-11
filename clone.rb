bb2cfddfd9fbdf20e210601c6617c5fa6d763875
kubo39 <kubo39@gmail.com>
2015-01-12 02:21:02 +0900

    Update clone.rb

--- clone.rb
+++ clone.rb
@@ -2,20 +2,20 @@ require 'ffi'
 
 CLONE_NEWPID = 0x20000000
 
-module LinuxClone
+module Libc
   extend FFI::Library
   ffi_lib 'libc.so.6'
   callback :f_c, [:void], :int
   attach_function :clone, [:f_c, :pointer, :int], :int
-  attach_function :getpid, [], :int
+  attach_function :getpid, [:void], :int
 end
 
 stack = FFI::MemoryPointer.new(:char, 8096)
 stack_top = FFI::Pointer.new(stack.address + 8096)
 
 f = proc do
-  puts "getpid: #{LinuxClone.getpid}"
+  puts "getpid: #{Libc.getpid}"
   return 0
 end
 
-puts "clone(2) retval= #{LinuxClone.clone(f, stack_top, CLONE_NEWPID)}"
+puts "clone(2) retval= #{Libc.clone(f, stack_top, CLONE_NEWPID)}"
